1bed83c2953529c6419181f06f9e5c30249a9662
CreateWebSiteTeam <yourgitemail@example.com>
2022-11-23 04:04:32 +0900

    仮swiper実装

--- src/components/swiper/SwiperAnimation.tsx
+++ src/components/swiper/SwiperAnimation.tsx
@@ -1,34 +1,56 @@
 import React, { useRef, useState } from "react"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react"
-import { EffectFade, Navigation, Pagination } from "swiper"
+import { EffectFade, Navigation, Pagination,Autoplay } from "swiper"
 // Import Swiper styles
 import "swiper/css"
 import "swiper/css/effect-fade"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
 import styles from '@/components/swiper/index.module.css'
+import { delay } from "@reduxjs/toolkit/dist/utils"
 // export const SwiperItem = () => {
 //   return (
 
 //   )
 // }
-
 export const SwiperContainer = () => {
+  const ref = useRef<HTMLElement>(null);
+  const [currentIndex,setCurrentIndex] = useState<number>();
+  const prevRef = useRef<HTMLDivElement>(null)
+  const nextRef = useRef<HTMLDivElement>(null)
+  let speed = 3000;
+  console.log(Swiper)
   return (
     <div className={styles.swiper_in}>
     <Swiper
+          loop={true}
           spaceBetween={30}
-          effect={"fade"}
-          navigation={true}
-          pagination={{
-            clickable: true,
+          navigation={{
+            prevEl: "#button_prev",
+            nextEl: "#button_next",
           }}
-          modules={[EffectFade, Navigation, Pagination]}
+          autoplay={{
+            disableOnInteraction:false
+          }}
+          onSlideChange = {(e) => setCurrentIndex(e.realIndex + 1)}
+          onSlideChangeTransitionStart = {() => {
+            if(ref.current) {
+              ref.current.style.transitionDuration = '0s'
+              ref.current.style.transform = 'scaleX(0)'
+            }
+          }}
+          onSlideChangeTransitionEnd = {() => {
+            if(ref.current) {
+              ref.current.style.transitionDuration = speed + 'ms'
+              ref.current.style.transform = 'scaleX(1)'
+            }
+          }}
+          modules={[EffectFade, Navigation, Pagination,Autoplay]}
           className={styles.swiper}
         >
           <SwiperSlide style={{width:'100%'}}>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
             <span>ああああ</span>
           </SwiperSlide>
           <SwiperSlide>
@@ -40,8 +62,23 @@ export const SwiperContainer = () => {
           <SwiperSlide>
             <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
           </SwiperSlide>
-          <div className="swiper-pagination"></div>
         </Swiper>
+        <div className={styles.navigation}>
+          <div className={styles.pagination_button}>
+            <div id="button_prev" className={`swiper-button-prev ${styles.swiper_prev}`}></div>
+            <div id="button_next" className={`swiper-button-next ${styles.swiper_next}`}></div>
+          </div>
+          <div className={styles.pagination_area}>
+            <div className={styles.current_index}>
+              <span>0{currentIndex}</span></div>
+            <div className={styles.progressbar}>
+              <span className={styles.progressbar_in} ref={ref}></span>
+            </div>
+            <div className={styles.all_index}>
+              <span>04</span>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
