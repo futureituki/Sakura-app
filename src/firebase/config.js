60cce781fa72179717cd47e2f2af841e715a12c5
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-01 03:13:41 +0900

    #4 firebase-config signupとlogin機能 redux-toolkit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA_BjARKqv_8auHX_ghQt4zTsMmRiAkAM0",
+  authDomain: "portofolio-fb1b5.firebaseapp.com",
+  projectId: "portofolio-fb1b5",
+  storageBucket: "portofolio-fb1b5.appspot.com",
+  messagingSenderId: "625400774816",
+  appId: "1:625400774816:web:0c2be4c8aa9dde43e749c5",
+  measurementId: "G-J5L2TN0FFV"
 }
