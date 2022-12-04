a0478aa1c02114c5c789e9dc087ec0c3076933bf
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-04 15:45:44 +0900

    #5 画像追加

--- src/components/templates/Mypage.tsx
+++ src/components/templates/Mypage.tsx
@@ -22,7 +22,6 @@ export const MyPage = () => {
   if (user.first_favorite) {
     name = user.first_favorite['name' as any]
     src = user.first_favorite['src' as any]
-    // src = user.first_favorite?.get('name')
   }
   console.log(user)
   useEffect(() => {
@@ -63,32 +62,35 @@ export const MyPage = () => {
               </div>
             </div>
           </SwiperSlide>
+          {/* {user.favorite.map((member,index) => (
+            <SwiperSlide style={{ width: '56%' }}>
+              <div className={styles.member_box}>
+                <Image
+                  height={110}
+                  width={100}
+                  // src={`${member['src' as any] ? member['src'] : '/no-image-person'}`}
+                  src={`/no-image-person.jpeg`}
+                  alt={'の画像'}
+                  className={styles.member_img}
+                />
+                <div className={styles.member_information}>
+                  <TextLabel color='#ccc'>気になる</TextLabel>
+                  <Typography fontSize={14}>藤吉夏鈴</Typography>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))} */}
           <SwiperSlide style={{ width: '56%' }}>
             <div className={styles.member_box}>
               <Image
                 height={110}
                 width={100}
-                src={'/assets/member/fujiyoshi_main_visual.jpeg'}
+                src={`/no-image-person.jpeg`}
                 alt={'の画像'}
                 className={styles.member_img}
               />
               <div className={styles.member_information}>
                 <TextLabel color='#ccc'>気になる</TextLabel>
-                <Typography fontSize={14}>藤吉夏鈴</Typography>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide style={{ width: '56%' }}>
-            <div className={styles.member_box}>
-              <Image
-                height={110}
-                width={100}
-                src={'/assets/member/fujiyoshi_main_visual.jpeg'}
-                alt={'の画像'}
-                className={styles.member_img}
-              />
-              <div className={styles.member_information}>
-                <TextLabel color='#ff69b8'>推しメン</TextLabel>
                 <Typography fontSize={14}>藤吉夏鈴</Typography>
               </div>
             </div>
