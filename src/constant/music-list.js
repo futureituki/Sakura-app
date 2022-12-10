be6de26c461e883ccefbb9ee58438ae202e2358f
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-11 02:49:52 +0900

    #10 format

--- src/constant/music-list.js
+++ src/constant/music-list.js
@@ -1,5 +1,10 @@
 export const musicList = [
-  { src: '/assets/music/masatu-player.m4a', name: '摩擦係数', img: '/assets/disc/masatu.jpeg', number:"" },
+  {
+    src: '/assets/music/masatu-player.m4a',
+    name: '摩擦係数',
+    img: '/assets/disc/masatu.jpeg',
+    number: '',
+  },
   {
     src: '/assets/music/samidare-player.m4a',
     name: '五月雨よ',
@@ -12,7 +17,12 @@ export const musicList = [
     img: '/assets/disc/nagaredama.jpeg',
     number: '3rd',
   },
-  { src: '/assets/music/ban-player.m4a', name: 'BAN', img: '/assets/disc/ban.jpeg', number: '2nd' },
+  {
+    src: '/assets/music/ban-player.m4a',
+    name: 'BAN',
+    img: '/assets/disc/ban.jpeg',
+    number: '2nd',
+  },
   {
     src: '/assets/music/nobodys-player.m4a',
     name: 'Nobody`s fault',
