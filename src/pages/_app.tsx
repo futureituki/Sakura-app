55b59c05e587919cfd851a17b7eb6eee00a6d196
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-14 23:35:28 +0900

    #7 表示速度改善

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import '@/styles/globals.scss'
 import '@/components/atoms/Heading/index.css'
 import type { AppPropsWithLayout } from 'next/app'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '@/redux/store'
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({ Component, pageProps, router }: AppPropsWithLayout) {
+  let home = false
+  if (router.pathname == '/') home = true
   const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(
-    <Provider store={store}>
-      <Component {...pageProps} />
-      <script></script>
-    </Provider>,
-  )
+  return home
+    ? getLayout(<Component {...pageProps} />)
+    : getLayout(
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>,
+      )
 }
