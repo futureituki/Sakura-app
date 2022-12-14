705990455a0cc209d82a6d98267d91d94985762b
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-15 01:16:54 +0900

    next/imageからimgタグに変更

--- src/components/Site/Components/Section-Img.tsx
+++ src/components/Site/Components/Section-Img.tsx
@@ -7,5 +7,5 @@ type Props = {
   height: number
 }
 export const SectionImg: FC<Props> = ({ src, alt_name, width, height }) => {
-  return <img src={src} alt={`${alt_name}の画像`} style={{"width":width, "height":height}} />
+  return <img src={src} alt={`${alt_name}の画像`} style={{"width":`${width}+px` + "px", "height":`${height}+px`}} />
 }
