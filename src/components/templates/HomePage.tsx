f3197ef17f1a0fec243408af81f00eeb18b442d1
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-14 16:39:47 +0900

    #7 dynamic import 表示速度向上

--- src/components/templates/HomePage.tsx
+++ src/components/templates/HomePage.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Section01 } from '../Site/Components/Section01'
+import dynamic from 'next/dynamic'
+import React, { Suspense } from 'react'
+import Section01 from '../Site/Components/Section01'
 import { Section02 } from '../Site/Components/Section02'
 import { Section03 } from '../Site/Components/Section03'
 import { Section04 } from '../Site/Components/Section04'
 import { Section05 } from '../Site/Components/Section05'
 import { Section06 } from '../Site/Components/Section06'
 
+const DynamicSection = dynamic(
+  () => {
+    return import('@/components/Site/Components/Section01')
+  },
+  {
+    suspense: true,
+  },
+)
 export const HomePage = () => {
   return (
     <div>
       <main style={{ background: '#000' }}>
+        <Suspense fallback={`Loading...`}>
+          <DynamicSection />
+        </Suspense>
         <Section01 />
         <Section02 />
         <Section03 />
