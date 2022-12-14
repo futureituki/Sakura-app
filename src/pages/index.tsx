22f1d704eddf87e75b6faf4a1a651c73c14b5a34
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-15 00:15:19 +0900

    #7 suspense test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { NextPageWithLayout } from 'next'
 import { HomePage } from '@/components/templates/HomePage'
 import { Layout } from '@/layout/Layout'
+import { Suspense } from 'react'
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
+    <Suspense fallback={'loading'}>
       <HomePage />
+    </Suspense>
     </>
   )
 }
