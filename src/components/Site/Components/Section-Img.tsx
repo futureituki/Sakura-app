0ef87c916e79f9485468893c677602c933bd8428
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-15 15:51:04 +0900

    #7 prettier-error

--- src/components/Site/Components/Section-Img.tsx
+++ src/components/Site/Components/Section-Img.tsx
@@ -4,9 +4,9 @@ import { FC } from 'react'
 type Props = {
   src: string
   alt_name: string
-  width:number
-  height:number
+  width: number
+  height: number
 }
 export const SectionImg: FC<Props> = ({ src, alt_name, width, height }) => {
-  return <Image src={src} alt={`${alt_name}の画像`} width={width} height={height}/>
+  return <Image src={src} alt={`${alt_name}の画像`} width={width} height={height} />
 }
