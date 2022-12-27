957ba90c7b8b78eebf6f4f2c73e813ac07e05e55
CreateWebSiteTeam <yourgitemail@example.com>
2022-12-27 21:08:56 +0900

    #7 firebase 環境変数設定

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,11 +2,11 @@ import { getApps, initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
-import { firebaseConfig } from '@/firebase/config'
-
+import firebaseConfig from '@/firebase/config'
 if (!getApps()?.length) {
   initializeApp(firebaseConfig)
 }
+console.log(firebaseConfig.apiKey)
 export const storage = getStorage()
 export const auth = getAuth()
 export const db = getFirestore()
